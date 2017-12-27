69aaba8dfd144be2de058219235fdaa146de2d52
Lena <fatmeow@gmail.com>
2017-12-26 19:45:33 -0500

    Complete step 7, share state of count request

--- ajax.js
+++ ajax.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var requestbtn = document.querySelector('#step12 button'),
       section = document.querySelector('#step3456'),
-      pingPongbtn = section.querySelector('button');
+      pingPongbtn = section.querySelector('button'),
+      countbtn = document.querySelector('#step7 button');
 
   requestbtn.addEventListener('click', function() {
     $.ajax({
@@ -33,5 +34,19 @@ document.addEventListener("DOMContentLoaded", function() {
     })
   })
 
+  countbtn.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text',
+    }).done(function (responseData) {
+      console.log(responseData);
+      var spanCount = document.createElement('span');
+      spanCount.innerText = responseData;
+      section = document.querySelector('#step7');
+      section.appendChild(document.createElement('br'));
+      section.appendChild(spanCount);
+    })
+  })
 
 });
